1ac31b1016095c091e3afec111224f1f934911e5
Ba Nguyen <b.pynguyen@gmail.com>
2021-05-17 14:23:09 +0700

    adding route to redirect non-admin user to customer page

--- src/pages/sellerpage/sellerPage.component.jsx
+++ src/pages/sellerpage/sellerPage.component.jsx
@@ -1,6 +1,7 @@
 import './sellerPage.styles.scss';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import SellerSidebar from '../../components/seller-sidebar/seller-sidebar.component';
 import SellerOrderPage from '../seller-orderpage/sellerOrderPage.component';
@@ -8,7 +9,10 @@ import SellerUserPanel from '../../components/seller-userpanel/sellerUserPanel.c
 import SellerProductViewPage from '../seller-productviewpage/sellerProductViewPage.component';
 import SellerProductCreatePage from '../seller-product-create-page/sellerProductCreatePage.component';
 const SellerPage = () => {
-  return (
+  const userRole = useSelector((state) => state.user.user.roles);
+  const isAdmin = userRole.includes('admin');
+  console.log('sellerPage', isAdmin);
+  return isAdmin ? (
     <div className="sellers-page">
       <SellerSidebar />
       <Switch>
@@ -24,6 +28,8 @@ const SellerPage = () => {
       </Switch>
       <SellerUserPanel />
     </div>
+  ) : (
+    <Redirect to="/customers" />
   );
 };
 
